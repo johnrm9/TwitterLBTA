d13ffb313ec432dfb516ac7da2123c92f9d7c90e
johnrm9 <johnrm9@gmail.com>
2017-12-18 13:28:42 -0800

    Initial Commit

---
+++ TwitterLBTA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitterLBTA
+//
+//  Created by John Martin on 12/18/17.
+//  Copyright © 2017 John Martin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
